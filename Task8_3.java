d0743b1a4235ffc4fdaceda7ce7536de747b15e8
Mohamed Shajan <emailshajan.er@gmail.com>
2024-06-03 23:49:48 +0530

    3rd Question

---
+++ Task8_3.java
@@ -0,0 +1,14 @@
+package guvi;
+
+public class Task8_3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (char ch = 'A'; ch <= 'Z'; ch++) {
+            System.out.print(ch + " ");
+        }
+    }
+
+	}
+
+
